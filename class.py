a8d761565bd6052cbd6c4f8cb2485c695310b353
mforv <master-forve@ya.ru>
2018-06-28 18:51:58 +0500

    Refactoring & Hierarchy update

--- class.py
+++ class.py
@@ -1,15 +1,50 @@
+#!usr/bin/python3
+# -*- coding: utf-8 -*-
+
 from lingv import *
 
 my = Mystem()
 
-with open('test.txt','r') as f:
-    str = [line.rstrip(r'\S*[\w]') for line in f][0]
+with open('test.txt','r', encoding="utf-8") as f:
+    string = [line.rstrip(r'\S*[\w]') for line in f][1]
 
-for a in my.analyze(str):
+class HierarchyBuilder(object):
+    # def __init__(self):
+    #     self.analysis = analysis
+
+    def build(self, analysis):
+        pos = set(re.findall(r"^([A-Z]+).+", analysis[0]['gr']))
+        if "S" in pos:
+            return Существительное(analysis)
+        if "A" in pos:
+            return Прилагательное(analysis)
+        if "NUM" in pos:
+            return Числительное(analysis)
+        if "V" in pos:
+            return Глагол(analysis)
+        if "ADV" in pos:
+            return Наречие(analysis)
+        if "PRO" in pos:
+            return Местоимение(analysis)
+        if "CONJ" in pos:
+            return Союз(analysis)
+        if "INTJ" in pos:
+            return Междометие(analysis)
+        if "PR" in pos:
+            return Предлог(analysis)
+        if "PART" in pos:
+            return Частица(analysis)
+
+
+hb = HierarchyBuilder()
+for a in my.analyze(string):
     try:
-        x = Cлово(a['analysis'])
-        for y in [Существительное, Глагол, Прилагательное, Наречие]:
-            if x == y():
-                print(x)
+        x = hb.build(a['analysis'])
+        if x is not None:
+            print(x, x.pos)
+            if isinstance(x, Существительное):
+                print(x.падеж, x.род, x.число)
+            elif isinstance(x, Глагол):
+                print(x.время, x.залог, x.лицо, x.наклонение)
     except KeyError:
         pass
